aae8fb70cc97acbbe115034e6693511da36c0fe9
kiwon kim <rlarldnjs17@gmail.com>
2024-08-09 17:12:40 +0900

    feat: 로그인, 회원가입

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -21,9 +21,19 @@ class MainActivity : AppCompatActivity() {
             var etId = binding.etLogid
             var etPw = binding.etLogpw
 
+            // 아이디 or 비밀번호를 입력하지 않는 경우
             if(etId.text.isEmpty() || etPw.text.isEmpty()) {
                 Toast.makeText(this, "아이디/비밀번호를 입력해야 합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+
+            // 아이디 or 비밀번호가 db에 없거나 일치하지 않는 경우
+//            else if(etId)
+
+            else Toast.makeText(this, "로그인 성공!", Toast.LENGTH_SHORT).show()
+
+            intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
 
         binding.btnRes.setOnClickListener {
